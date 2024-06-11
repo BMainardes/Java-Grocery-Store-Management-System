599d8ef1388ae1b324a6cfab257b2579c7bd2402
FabrIcioM0r3ir4 <130069865+FabrIcioM0r3ir4@users.noreply.github.com>
2024-06-11 17:11:07 -0300

    Update bdmercado.sql

--- bdmercado.sql
+++ bdmercado.sql
@@ -1,3 +1,20 @@
+-- --------------------------------------------------------
+-- Servidor:                     127.0.0.1
+-- Versão do servidor:           10.4.32-MariaDB - mariadb.org binary distribution
+-- OS do Servidor:               Win64
+-- HeidiSQL Versão:              12.6.0.6765
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
 -- Copiando estrutura do banco de dados para bdmercado
 CREATE DATABASE IF NOT EXISTS `bdmercado` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
 USE `bdmercado`;
@@ -33,16 +50,16 @@ INSERT INTO `produtos` (`id`, `nome`, `descricao`, `preco`, `quantidade_estoque`
 CREATE TABLE IF NOT EXISTS `vendas` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `vendedor_id` int(11) DEFAULT NULL,
-  `nome_cliente` varchar(100) DEFAULT NULL,
-  `data_venda` datetime DEFAULT current_timestamp(),
-  `valor_total` decimal(10,2) DEFAULT NULL,
+  `cpf` varchar(20) DEFAULT NULL,
+  `data_venda` datetime NOT NULL DEFAULT current_timestamp(),
+  `valor_total` decimal(10,2) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `vendedor_id` (`vendedor_id`),
   CONSTRAINT `vendas_ibfk_1` FOREIGN KEY (`vendedor_id`) REFERENCES `vendedor` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- Copiando dados para a tabela bdmercado.vendas: ~3 rows (aproximadamente)
-INSERT INTO `vendas` (`id`, `vendedor_id`, `nome_cliente`, `data_venda`, `valor_total`) VALUES
+INSERT INTO `vendas` (`id`, `vendedor_id`, `cpf`, `data_venda`, `valor_total`) VALUES
 	(1, 1, NULL, '2024-06-08 16:42:59', 22.00),
 	(2, 2, NULL, '2024-06-08 16:42:59', 8.99),
 	(3, 1, NULL, '2024-06-08 16:42:59', 15.00);
@@ -52,7 +69,7 @@ CREATE TABLE IF NOT EXISTS `venda_produto` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `venda_id` int(11) DEFAULT NULL,
   `produto_id` int(11) DEFAULT NULL,
-  `quantidade` int(11) NOT NULL,
+  `quantidade` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `venda_id` (`venda_id`),
   KEY `produto_id` (`produto_id`),
@@ -85,10 +102,10 @@ INSERT INTO `vendedor` (`id`, `nome`, `cpf`, `telefone`, `email`) VALUES
 	(2, 'Matheus Visoto', '09876543210', '11888888888', 'matheus@fatec.sp.gov.br'),
 	(3, 'Fabricio', '09876543220', '12000000000', 'fabricio@fatec.sp.gov.br');
 
--- Criando o gatilho after_venda_produto_insert para atualizar a quantidade em estoque
+-- Copiando estrutura para trigger bdmercado.after_venda_produto
+SET @OLDTMP_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO';
 DELIMITER //
-
-CREATE TRIGGER after_venda_produto_insert
+CREATE TRIGGER after_venda_produto
 AFTER INSERT ON venda_produto
 FOR EACH ROW
 BEGIN
@@ -96,8 +113,12 @@ BEGIN
     UPDATE produtos
     SET quantidade_estoque = quantidade_estoque - NEW.quantidade
     WHERE id = NEW.produto_id;
-END;
-
-//
-
+END//
 DELIMITER ;
+SET SQL_MODE=@OLDTMP_SQL_MODE;
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
